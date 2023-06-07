f8d099b35febcc75b8bf0ba650702ee32816633e
alexanderdz27 <alexanderdz127@gmail.com>
2023-06-07 16:39:41 +0700

    update1

---
+++ app/Models/About.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+class About
+{
+    private static $About_team = [
+        [
+            "image" => "image/foto_atam.jpeg",
+            "name" => "Muhammad Khatam Ramadhan",
+            "ig" =>"RP.100.000",
+            "fb" => ""
+        ],
+        [
+            "image" => "image/kartun.png",
+            "name" => "Muhammad Alexander Dzulkarnain",
+            "ig" =>"RP.100.000",
+            "fb" => ""
+        ],
+        [
+            "image" => "image/foto_bagas.jpeg",
+            "name" => "Bagas Arjun Valentino",
+            "ig" =>"RP.100.000",
+            "fb" => ""
+        ]
+    ];
+
+    public static function all()
+    {
+        return collect(self::$About_team);
+    }
+
+    public static function find($slug)
+    {
+        $products_bucket=  static::all();
+        return $products_bucket-> firstWhere('slug', $slug);
+    }
+}
